094cf703a9aabe2923619536f89e8d11249732df
ainesmile <ainesmile@gmail.com>
2017-05-29 15:18:43 +1200

    create model `commodity` as abstract base class

---
+++ deltago/models/commodity.py
@@ -0,0 +1,48 @@
+from __future__ import unicode_literals
+
+from django.db import models
+from django.utils import timezone
+
+class Commodity(models.Model):
+    BABY = 'B'
+    FOOD = 'F'
+    HEALTH = 'H'
+    SPECIAL = 'S'
+    CATEGORY = (
+        (BABY, 'Baby')
+        (FOOD, 'Food'),
+        (HEALTH, 'Health'),
+        (SPECIAL, 'Special'),
+    )
+
+    number = models.IntegerField(
+        unique=True)
+    name = models.CharField(
+        max_length=128,
+        unique=True)
+    brand = models.CharField(max_length=128)
+    origin = models.CharField(
+        max_length=128,
+        null=True,
+        blank=True)
+    price = models.IntegerField()
+    discount = models.IntegerField(
+        blank=True,
+        null=True)
+    pic_url = models.URLField(
+        blank=True,
+        null=True)
+    category = models.IntegerField(
+        choices=CATEGORY
+        default=CATEGORY.BABY)
+    description = models.TextField(
+        default=name)
+    online_date = models.DateTimeField(
+        default = timezone.now)
+
+    class Meta:
+        abstract = True
+        ordering = ['brand', 'name']
+
+    def __str__(self):
+        return self.name
