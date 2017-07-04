ced9dbb4a934804abb2c76a29a28e70cb5fb178c
ainesmile <ainesmile@gmail.com>
2017-07-04 22:40:33 +1200

    draft page `medicinal`

--- deltago/urls/pages.py
+++ deltago/urls/pages.py
@@ -4,4 +4,5 @@ from deltago.views import pages, babycare
 urlpatterns = [
     url(r'^$', pages.index, name='index'),
     url(r'^babycare$', babycare.index, name='babycare'),
+    url(r'^babycare/medicinal$', babycare.medicinal, name='medicinal'),
 ]
--- deltago/views/babycare.py
+++ deltago/views/babycare.py
@@ -3,8 +3,16 @@ from services.share import pagination
 from deltago.models import BabyCare
 
 def index(request):
-    sub_category = "F"
-    products = BabyCare.objects.filter(sub_category=sub_category)
+    products = BabyCare.objects.filter(sub_category="F")
+    page = request.GET.get('page', 1)
+    paginations = pagination(products, page, 20)
+    return render(request, 'deltago/babycare/index.html', {
+        "products": paginations,
+        "paginations": paginations,
+        })
+
+def medicinal(request):
+    products = BabyCare.objects.filter(sub_category="M")
     page = request.GET.get('page', 1)
     paginations = pagination(products, page, 20)
     return render(request, 'deltago/babycare/index.html', {
