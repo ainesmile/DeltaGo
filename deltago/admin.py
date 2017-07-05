d6b36bc6278a43f9f3daee7daf5a217174728004
ainesmile <ainesmile@gmail.com>
2017-07-06 11:06:37 +1200

    remove outdate model from admin

--- deltago/admin.py
+++ deltago/admin.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
-from deltago.models import commodity, nutrition
+from deltago.models import commodity
 
 admin.site.register(commodity.BabyCare)
 admin.site.register(commodity.BabyCareDetails)
-admin.site.register(nutrition.Nutrition)
