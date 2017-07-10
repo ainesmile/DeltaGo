cd1cb2591c113a43e55c18aa7ac632f6dac7da43
ainesmile <ainesmile@gmail.com>
2017-07-11 11:06:57 +1200

    delete unnecessary codes

--- deltago/models/search.py
+++ deltago/models/search.py
@@ -3,20 +3,6 @@ from __future__ import unicode_literals
 from django.db import models
 
 class Search(models.Model):
-    BABYCARE = 'B'
-    FOOD = 'F'
-    SUPPLEMENT = 'P'
-    BEAUTY = 'U'
-    SPECIAL = 'S'
-
-    CATEGORY = (
-        (BABYCARE, 'BabyCare'),
-        (FOOD, 'Food'),
-        (SUPPLEMENT, 'Supplement'),
-        (BEAUTY, 'Beauty'),
-        (SPECIAL, 'Special'),
-    )
-
     name = models.CharField(max_length = 128)
     stockcode = models.CharField(max_length = 128, unique = True)
     model_name = models.CharField(max_length = 128)
--- deltago/urls/pages.py
+++ deltago/urls/pages.py
@@ -1,5 +1,5 @@
 from django.conf.urls import url
-from deltago.views import pages, babycare
+from deltago.views import pages
 
 urlpatterns = [
     url(r'^$', pages.index, name='index'),
