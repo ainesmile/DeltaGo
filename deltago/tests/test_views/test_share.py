ff5fa41f1f43a49879ff0d986e8ac70cf13bfbb4
ainesmile <ainesmile@gmail.com>
2017-07-10 13:39:13 +1200

    search based on content

---
+++ deltago/tests/test_views/test_share.py
@@ -0,0 +1,20 @@
+from django.test import TestCase
+from deltago.models.search import Search
+
+from deltago.views.services import share
+
+class PagesViewTest(TestCase):
+    fixtures = [
+        'deltago/fixtures/search.json',
+        'deltago/fixtures/babycare.json',
+        'deltago/fixtures/babycaredetails.json'
+    ]
+
+    def test_search(self):
+        content = "Banana Avocado"
+        expected = [2469]
+        results = share.search(content)
+        pks = []
+        for r in results:
+            pks.append(r.pk)
+        self.assertEqual(pks, expected)
--- deltago/views/services/share.py
+++ deltago/views/services/share.py
@@ -1,4 +1,7 @@
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.apps import apps
+from deltago.models.search import Search
+from deltago.models.commodity import BabyCare
 
 def pagination(items, page, paginate_by):
     paginator = Paginator(items, paginate_by)
@@ -9,3 +12,15 @@ def pagination(items, page, paginate_by):
     except EmptyPage:
         text = paginator.page(paginator.num_pages)
     return text
+
+def search(content):
+    results = []
+    search_filters = Search.objects.filter(name__contains=content)
+    for item in search_filters:
+        stockcode = item.stockcode
+        model_name = item.model_name
+        app_model = apps.get_model(app_label='deltago', model_name=model_name)
+        list = app_model.objects.filter(stockcode=stockcode)
+        if len(list) != 0:
+            results.append(list[0])
+    return results
