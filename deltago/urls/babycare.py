c5fef1c2ff37cb44e04f53baf62e1aa074998c9a
ainesmile <ainesmile@gmail.com>
2017-07-11 11:06:20 +1200

    split babycare url patterns into a seperate file

---
+++ deltago/urls/babycare.py
@@ -0,0 +1,10 @@
+from django.conf.urls import url
+from deltago.views import babycare
+
+urlpatterns = [
+    url(r'^babycare/babyfood-from-(?P<month>\w+)-mths/$', babycare.food, name='food'),
+    url(r'^babycare/medicinal$', babycare.medicinal, name='medicinal'),
+    url(r'^babycare/skincare$', babycare.skincare, name='skincare'),
+    url(r'^babycare/nappy$', babycare.nappy, name='nappy'),
+    url(r'^babycare/(?P<stockcode>\w+)/$', babycare.details, name='details'),
+]
