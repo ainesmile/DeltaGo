5f780afbc247e8a4c9ef29744ca68abc6f9f6ba4
ainesmile <ainesmile@gmail.com>
2017-07-16 21:11:11 +1200

    create models about order

--- deltago/admin.py
+++ deltago/admin.py
@@ -2,8 +2,12 @@ from django.contrib import admin
 from deltago.models import commodity
 from deltago.models import search
 from deltago.models import cart
+from deltago.models import order
 
 admin.site.register(commodity.BabyCare)
 admin.site.register(commodity.BabyCareDetails)
 admin.site.register(search.Search)
 admin.site.register(cart.Cart)
+admin.site.register(order.Order)
+admin.site.register(order.Ship)
+admin.site.register(order.Payment)
---
+++ deltago/models/order.py
@@ -0,0 +1,86 @@
+from __future__ import unicode_literals
+
+from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
+
+
+
+class Order(models.Model):
+    UNPAID = 'U'
+    ARCHIVED = 'A'
+    PROCESSING = 'P'
+    CANCELING = 'C'
+    FINISHED = 'F'
+    STATES = (
+        (UNPAID, 'Unpaid'),
+        (ARCHIVED, 'Archived'),
+        (PROCESSING, 'Processing'),
+        (CANCELING, 'Canceling'),
+        (FINISHED, 'Finished')
+    )
+
+    commodities = models.CharField(max_length=500)
+    prices = models.CharField(max_length=500)
+    quantities = models.CharField(max_length=250)
+    state = models.CharField(max_length=2, choices=STATES, default=UNPAID)
+    
+    payment_method = models.CharField(max_length=128)
+    ship_address = models.CharField(max_length=200)
+    
+    exchange_rate = models.IntegerField()
+    subtotal = models.IntegerField()
+    total = models.IntegerField()
+    
+    user = models.ForeignKey(User)
+    
+
+    # unpaind_time is order's created_time
+    unpaind_time = models.DateField(default=timezone.now)
+    archived_time = models.DateField(null = True, blank = True)
+    processing_time = models.DateField(null = True, blank = True)
+    canceling_time = models.DateField(null = True, blank = True)
+    finished_time = models.DateField(null = True, blank = True)
+
+    class Meta:
+        ordering = ['-unpaind_time']
+
+class Ship(models.Model):
+    SEND = 'S'
+    PROCESSING = 'P'
+    DELIVERED = 'D'
+
+    STATES = (
+        (SEND, 'Send'),
+        (PROCESSING, 'Processing'),
+        (DELIVERED, 'Delivered')
+    )
+
+    order = models.ForeignKey(Order)
+    number = models.CharField(max_length=128)
+    express = models.CharField(max_length=128)
+    linker = models.URLField()
+    fee = models.IntegerField()
+    address = models.CharField(max_length=128)
+    receiver = models.CharField(max_length=128)
+    signer = models.CharField(max_length=128)
+
+    send_time = models.DateField(default=timezone.now)
+    processing_time = models.DateField(default=timezone.now)
+    delivered_time = models.DateField(default=timezone.now)
+    def __str__(self):
+        return self.number
+
+
+class Payment(models.Model):
+    order = models.ForeignKey(Order)
+    name = models.CharField(max_length=128)
+    number = models.CharField(max_length=200)
+    method = models.CharField(max_length=128)
+    ship_fee = models.IntegerField()
+    subtotal = models.IntegerField()
+    currency_unit = models.CharField(max_length=128)
+    exchange_rate = models.IntegerField()
+    created_time = models.DateField(default=timezone.now)
+    def __str__(self):
+        return self.order
