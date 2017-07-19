0caa69211904997468ea84bcb94d87403f883c57
ainesmile <ainesmile@gmail.com>
2017-07-19 16:56:33 +1200

    abstruct functions to save commodity, details, and search records

--- deltago/commodity_data/countdown/create.py
+++ deltago/commodity_data/countdown/create.py
@@ -19,3 +19,17 @@ def create(item, fields, model):
 def save(items, fields, model):
     for item in items:
         create(item, fields, model)
+
+def commodity(items):
+    fields = ["name", "volume_size", "price", "was_price", "special_price",
+        "category", "sub_category", "stockcode"]
+    save(items, fields, Commodity)
+
+def details(items):
+    fields = ["commodity", "pic_url", "description",
+        "nutrition", "ingredient", "claim", "endorsement"]
+    save(items, fields, Details)
+
+def search(items):
+    fields = ["name", "commodity"]
+    save(items, fields, Search)
