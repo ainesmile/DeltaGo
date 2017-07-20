7dcd6be4c6b5836dedcda51e1fa974fdb905fe85
ainesmile <ainesmile@gmail.com>
2017-07-20 17:00:25 +1200

    create command `save` to save data to db

--- deltago/management/commands/save.py
+++ deltago/management/commands/save.py
@@ -1,17 +1,17 @@
 from django.core.management import BaseCommand
 import json
-from deltago.commodity_data.countdown.create import save_details, save_babycare, save_search
+from deltago.commodity_data.countdown.create import commodity, details, search
 
 class Command(BaseCommand):
-    help = 'Save data to datebase'
+    help = 'Save data to database'
 
     def handle(self, *args, **options):
-        with open('deltago/commodity_data/countdown/babycare_details.json') as babycare_details_data_file:
-            babycare_details = json.load(babycare_details_data_file)
-            
         with open('deltago/commodity_data/countdown/babycare.json') as babycare_data_file:
             babycare = json.load(babycare_data_file)
+            
+        with open('deltago/commodity_data/countdown/babycare_details.json') as babycare_details_data_file:
+            babycare_details = json.load(babycare_details_data_file)
         
-        save_babycare(babycare)
-        save_details(babycare_details)
-        save_search('BabyCare')
+        commodity(babycare)
+        details(babycare_details)
+        search()
