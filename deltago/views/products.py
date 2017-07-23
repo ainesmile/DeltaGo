7ca187bf3a0d2465a64791e13be51564ecb810f6
ainesmile <ainesmile@gmail.com>
2017-07-23 19:14:29 +1200

    rename `commodity.base` and `commodity.products

--- deltago/views/products.py
+++ deltago/views/products.py
@@ -5,4 +5,5 @@ from deltago.views.services.products import sub
 def sub_category(request, categ_name, sub_categ_name):
     page = request.GET.get("page", 1)
     data = sub(categ_name, sub_categ_name, page, 20)
-    return render(request, 'deltago/commodity/base.html', data)
+    return render(request, 'deltago/commodity/products.html', data)
+    
