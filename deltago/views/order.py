a59ba7b66c04f08dad6b7fe488fe27dfb33d4043
ainesmile <ainesmile@gmail.com>
2017-08-03 22:34:36 +1200

    draft order page

--- deltago/views/order.py
+++ deltago/views/order.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 
 from deltago.exceptions import errors
-from deltago.views.services.order import order_list
+from deltago.views.services.order import generate_order, order_list, get_order_details
 
 
 def checkout(request):
@@ -12,7 +12,7 @@ def checkout(request):
         quantities = request.POST.getlist('quantity')
         checkboxes = request.POST.getlist('checkbox')
         try:
-            order.generate_order(user, checkboxes, quantities)
+            generate_order(user, checkboxes, quantities)
         except errors.EmptyCartError:
             print 'please choose at least one item'
     return redirect('order')
@@ -23,3 +23,9 @@ def order(request):
     page = request.GET.get('page', 1)
     data = order_list(user, page, 20)
     return render(request, 'deltago/order/order.html', data)
+
+def order_details(request, order_id):
+    # user = request.user
+    user = User.objects.get(pk=1)
+    order_details = get_order_details(order_id)
+    return render(request, 'deltago/order/details.html', {"order": order_details})
