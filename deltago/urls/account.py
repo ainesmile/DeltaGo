26279f85b8f7fa84c84d4a46cf83ff7581dc2a83
ainesmile <ainesmile@gmail.com>
2017-08-10 16:38:24 +1200

    reset password

--- deltago/urls/account.py
+++ deltago/urls/account.py
@@ -5,4 +5,6 @@ from deltago.views import account
 urlpatterns = [
     url(r'^login/$', account.login_view, name='login'),
     url(r'^logout/$', auth_views.logout, {'next_page': '/'}, name='logout'),
+    url(r'^password/reset$', account.password_reset_view, name='password_reset'),
+    url(r'^password/reset/done$', account.password_reset_done_view, name='password_reset_done'),
 ]
--- deltago/views/account.py
+++ deltago/views/account.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login
+from django.contrib.auth.views import password_reset
 
 def login_view(request):
     if request.method == "POST":
@@ -14,3 +15,14 @@ def login_view(request):
             return render(request, 'deltago/account/login.html', {"wrong": True}) 
     else:
         return render(request, 'deltago/account/login.html')
+
+def password_reset_view(request):
+    if request.method == 'POST':
+        request.session['password_reset_email'] = request.POST.get('email')
+    template_name = 'deltago/registration/password_reset_form.html',
+    return password_reset(request, template_name=template_name)
+
+def password_reset_done_view(request):
+    email = request.session['password_reset_email']
+    return render(request, 'deltago/registration/password_reset_done.html', {'email': email})
+
