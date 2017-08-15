92698b729308c743e3880743a864a4780525f90e
ainesmile <ainesmile@gmail.com>
2017-08-15 22:31:01 +1200

    rename file `products` to `product_service`

--- deltago/tests/test_views/test_products.py
+++ deltago/tests/test_views/test_products.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
 from django.test import TestCase, Client
 from django.core.paginator import Page
-from deltago.views.services import products
 
 from deltago.models import Commodity, Details
+from deltago.views.services import product_service
 
 
 class ProductsViewTest(TestCase):
@@ -57,24 +57,24 @@ class ProductsViewTest(TestCase):
 
 
     def test_get_categ(self):
-        categ = products.get_categ("babycare")
+        categ = product_service.get_categ("babycare")
         self.assertEqual(categ, 'B')
 
     def test_get_sub_categ(self):
-        sub_categ = products.get_sub_categ("babycare", "baby-food-from-4-mths")
+        sub_categ = product_service.get_sub_categ("babycare", "baby-food-from-4-mths")
         self.assertEqual(sub_categ, 'F4')
 
     def test_get_categs(self):
-        categs = products.get_categs("babycare", "baby-food-from-4-mths")
+        categs = product_service.get_categs("babycare", "baby-food-from-4-mths")
         self.assertEqual(categs, ('B', 'F4'))
 
     def test_get_sub_navs(self):
-        sub_navs = products.get_sub_navs("babycare")
+        sub_navs = product_service.get_sub_navs("babycare")
         self.assertEqual(sub_navs, self.e_sub_navs)
 
     def test_sub(self):
         page = self.request.GET.get("page", 1)
-        data = products.sub("babycare", "baby-food-from-4-mths", page, 20)
+        data = product_service.sub("babycare", "baby-food-from-4-mths", page, 20)
         self.assertTrue(isinstance(data["products"], Page))
         self.assertTrue(isinstance(data["paginations"], Page))
         self.assertTrue(data["empty_tips"], "暂无商品，待上架。")
@@ -88,14 +88,14 @@ class ProductsViewTest(TestCase):
             (None, None)
         ]
         for d, r in data:
-            self.assertEqual(products.json_loads(d), r)
+            self.assertEqual(product_service.json_loads(d), r)
             
     def test_get_nutritions(self):
-        result = products.get_nutritions(self.nutrition)
+        result = product_service.get_nutritions(self.nutrition)
         self.assertEqual(result, self.e_nutritions)
 
     def test_get_details(self):
-        data = products.get_details(1)
+        data = product_service.get_details(1)
         self.assertEqual(data["product"], self.product1)
         self.assertEqual(data["details"], self.details1)
         self.assertEqual(data["nutritions"], self.e_nutritions)
--- deltago/views/products.py
+++ deltago/views/products.py
@@ -1,13 +1,16 @@
 from django.shortcuts import render, redirect
 
-from deltago.views.services.products import sub, get_details
+from deltago.views.services import product_service
+
+def products(request):
+    pass
 
 def sub_category(request, categ_name, sub_categ_name):
     page = request.GET.get("page", 1)
-    data = sub(categ_name, sub_categ_name, page, 20)
+    data = product_service.sub(categ_name, sub_categ_name, page, 20)
     return render(request, 'deltago/commodity/products.html', data)
 
 def details(request, product_id):
-    data = get_details(product_id)
+    data = product_service.get_details(product_id)
     return render(request, 'deltago/commodity/details.html', data)
     
