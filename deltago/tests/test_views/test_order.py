0d9ee60935e3f19acf7b126a8620e703a0e6d7eb
ainesmile <ainesmile@gmail.com>
2017-08-15 22:21:09 +1200

    rename file `order` to `order_service`

--- deltago/tests/test_views/test_order.py
+++ deltago/tests/test_views/test_order.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 from deltago.exceptions import errors
 from deltago.models import Cartship, Cart, Commodity, Order
 
-from deltago.views.services import order
+from deltago.views.services import order_service
 
 SHIP_FEE = 500
 
@@ -53,53 +53,53 @@ class OrderViewTest(TestCase):
     def test_undelete_cartship(self):
         cartshipes = [self.cartship1, self.cartship2]
         for cartship in cartshipes:
-            order.undelete_cartship(cartship)
+            order_service.undelete_cartship(cartship)
             self.assertFalse(cartship.is_deleted)
 
     def test_choose_cartship(self):
-        order.choose_cartship(self.cartship1)
+        order_service.choose_cartship(self.cartship1)
         self.assertTrue(self.cartship1.is_chosen)
 
     def test_get_chosen_cartshipes(self):
-        chosens = order.get_chosen_cartshipes(self.checkboxes)
+        chosens = order_service.get_chosen_cartshipes(self.checkboxes)
         self.assertEqual(chosens, [self.cartship1])
 
     def test_get_unchosen_cartshipes(self):
         chosens = [self.cartship1]
-        unchosen = order.get_unchosen_cartshipes(self.all_cartshipes, chosens)
+        unchosen = order_service.get_unchosen_cartshipes(self.all_cartshipes, chosens)
         self.assertEqual(unchosen, [self.cartship2])
 
     def test_update_quantities(self):
         quantities = self.quantities
-        order.update_quantities(self.all_cartshipes, quantities)
+        order_service.update_quantities(self.all_cartshipes, quantities)
         for index, cartship in enumerate(self.all_cartshipes):
             quantity = quantities[index]
             self.assertEqual(cartship.quantity, quantity)
 
     def test_archive_cart(self):
-        order.archive_cart(self.cart)
+        order_service.archive_cart(self.cart)
         self.assertTrue(self.cart.is_archived)
 
     def test_new_cart_with_unchosens(self):
         unchosens = [self.cartship2]
-        order.new_cart_with_unchosens(self.admin, unchosens)
+        order_service.new_cart_with_unchosens(self.admin, unchosens)
 
     def test_get_price(self):
-        price = order.get_price(self.commodity)
+        price = order_service.get_price(self.commodity)
         self.assertEqual(price, self.price)
 
     def test_get_commodity_total(self):
-        commodity_total = order.get_commodity_total(self.cartship1)
+        commodity_total = order_service.get_commodity_total(self.cartship1)
         self.assertEqual(commodity_total, self.price)
 
     def test_get_subtotal(self):
-        subtotal = order.get_subtotal(self.all_cartshipes)
+        subtotal = order_service.get_subtotal(self.all_cartshipes)
         self.assertEqual(subtotal, 209*3)
 
     def test_init_order(self):
         subtotal = 209*3
         total = 209*3+500
-        new_order = order.init_order(self.cart, subtotal, total)
+        new_order = order_service.init_order(self.cart, subtotal, total)
         self.assertEqual(new_order.user, self.admin)
         self.assertEqual(new_order.subtotal, subtotal)
         self.assertEqual(new_order.total, total)
@@ -108,7 +108,7 @@ class OrderViewTest(TestCase):
         chosens = [self.cartship1]
         subtotal = 209
         total = 209 + 500
-        new_order = order.create_order_by_chosen(self.cart, chosens)
+        new_order = order_service.create_order_by_chosen(self.cart, chosens)
         self.assertEqual(new_order.user, self.cart.user)
         self.assertEqual(new_order.cart, self.cart)
         self.assertEqual(new_order.subtotal, subtotal)
@@ -120,7 +120,7 @@ class OrderViewTest(TestCase):
         checkboxes_data = [self.checkboxes, []]
         for checkboxes in checkboxes_data:
             try:
-                new_order = order.generate_order(user, checkboxes, quantities)
+                new_order = order_service.generate_order(user, checkboxes, quantities)
                 updated_cart = new_order.cart
                 self.assertTrue(updated_cart.is_archived)
                 self.assertEqual(new_order.user, user)
@@ -131,11 +131,11 @@ class OrderViewTest(TestCase):
 
 
     def test_get_order_fee(self):
-        fee = order.get_order_fee(self.order)
+        fee = order_service.get_order_fee(self.order)
         self.assertEqual(fee, self.e_fee)
 
     def test_get_commodity_table_item(self):
-        item = order.get_commodity_table_item(self.cartship1)
+        item = order_service.get_commodity_table_item(self.cartship1)
         self.assertEqual(item, self.e_commodity_table_item)
 
     def test_get_commodity_info_table(self):
@@ -143,7 +143,7 @@ class OrderViewTest(TestCase):
             (1, 2.09, 1, 2.09),
             (2, 2.09, 2, 4.18)
         ]
-        table = order.get_commodity_info_table(self.order)
+        table = order_service.get_commodity_info_table(self.order)
         for index, item in enumerate(table):
             data_item = data[index]
             self.assertEqual(item["commodity"].pk, data_item[0])
@@ -152,10 +152,10 @@ class OrderViewTest(TestCase):
             self.assertEqual(item["commodity_total"], data_item[3])
 
     def test_get_order_state_text(self):
-        text = order.get_order_state_text(self.order.state)
+        text = order_service.get_order_state_text(self.order.state)
         self.assertEqual(text, '未支付')
 
     def test_generate_order_serial_code(self):
-        serial_code = order.generate_order_serial_code(self.order)
+        serial_code = order_service.generate_order_serial_code(self.order)
         self.assertEqual(len(serial_code), 20)
 
--- deltago/views/order.py
+++ deltago/views/order.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
 
 from deltago.exceptions import errors
-from deltago.views.services.order import generate_order, order_list, get_order_details
+from deltago.views.services import order_service
 
 @login_required(login_url='login')
 def checkout(request):
@@ -12,7 +12,7 @@ def checkout(request):
         quantities = request.POST.getlist('quantity')
         checkboxes = request.POST.getlist('checkbox')
         try:
-            generate_order(user, checkboxes, quantities)
+            order_service.generate_order(user, checkboxes, quantities)
         except errors.EmptyCartError:
             print 'please choose at least one item'
     return redirect('order')
@@ -21,11 +21,11 @@ def checkout(request):
 def order(request):
     user = request.user
     page = request.GET.get('page', 1)
-    data = order_list(user, page, 20)
+    data = order_service.order_list(user, page, 20)
     return render(request, 'deltago/order/order.html', data)
 
 @login_required(login_url='login')
 def order_details(request, order_id):
     user = request.user
-    order_details = get_order_details(order_id)
+    order_details = order_service.get_order_details(order_id)
     return render(request, 'deltago/order/details.html', {"order": order_details})
