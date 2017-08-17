a33b4576581f2f25405ae13c9d971ac3aff36ab7
ainesmile <ainesmile@gmail.com>
2017-08-18 11:03:46 +1200

    modity url from `order` to `orders`

--- deltago/urls/order.py
+++ deltago/urls/order.py
@@ -4,5 +4,5 @@ from deltago.views import order
 urlpatterns = [
     url(r'^checkout$', order.checkout, name='checkout'),
     url(r'^orders/mine$', order.my_orders, name='my_orders'),
-    url(r'^order/details/(?P<order_id>\w+)$', order.order_details, name='order_details'),
+    url(r'^orders/details/(?P<order_id>\w+)$', order.order_details, name='order_details'),
 ]
