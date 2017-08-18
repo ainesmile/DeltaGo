9fc72b8f27251b0c36027395a09897bcd690348e
ainesmile <ainesmile@gmail.com>
2017-08-18 14:19:36 +1200

    get cartship subtotal in `share_service`

---
+++ deltago/tests/test_views/test_share.py
@@ -0,0 +1,48 @@
+from django.test import TestCase
+from django.contrib.auth.models import User
+
+from deltago.models import Commodity, Cartship
+
+from deltago.views.services import share_service
+
+class ShareServiceTest(TestCase):
+    fixtures = [
+        'deltago/fixtures/user.json',
+        'deltago/fixtures/commodity.json',
+        'deltago/fixtures/cart.json',
+        'deltago/fixtures/cartship.json',
+    ]
+
+    def setUp(self):
+        self.admin = User.objects.get(pk=1)
+        self.commodity1 = Commodity.objects.get(pk=1)
+        self.commodity7 = Commodity.objects.get(pk=7)
+
+        self.cartship1 = Cartship.objects.get(pk=1)
+        self.cartship2 = Cartship.objects.get(pk=2)
+
+    def test_get_commodity_price(self):
+        data = [
+            (self.commodity1, 209),
+            (self.commodity7, 599),
+        ]
+        for commodity, e_price in data:
+            price = share_service.get_commodity_price(commodity)
+            self.assertEqual(price, e_price)
+
+    def test_get_cartship_subtotal(self):
+        data = [
+            (self.cartship1, 209),
+            (self.cartship2, 209*2),
+        ]
+        for cartship, e_subtotal in data:
+            subtotal = share_service.get_cartship_subtotal(cartship)
+            self.assertEqual(subtotal, e_subtotal)
+
+    def test_get_cartshipes_subtotal(self):
+        cartshipes = [self.cartship1, self.cartship2]
+        e_subtotal = 209 + 209*2
+        subtotal = share_service.get_cartshipes_subtotal(cartshipes)
+        self.assertEqual(subtotal, e_subtotal)
+        
+        
--- deltago/views/services/share_service.py
+++ deltago/views/services/share_service.py
@@ -1,5 +1,6 @@
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
+
 def pagination(items, page, paginate_by):
     paginator = Paginator(items, paginate_by)
     try:
@@ -9,3 +10,23 @@ def pagination(items, page, paginate_by):
     except EmptyPage:
         text = paginator.page(paginator.num_pages)
     return text
+
+def get_commodity_price(commodity):
+    if commodity.price and commodity.price != "null":
+        price = commodity.price
+    else:
+        price = commodity.was_price
+    return float(price) * 100
+
+def get_cartship_subtotal(cartship):
+    commodity = cartship.commodity
+    price = get_commodity_price(commodity)
+    quantity = cartship.quantity
+    return price * quantity
+
+def get_cartshipes_subtotal(cartshipes):
+    subtotal = 0
+    for cartship in cartshipes:
+        subtotal += get_cartship_subtotal(cartship)
+    return subtotal
+
