48edb39219de711ef634cc4f015d7f4b7b50b387
ainesmile <ainesmile@gmail.com>
2017-08-18 16:33:51 +1200

    code refractoring about order_service

--- deltago/tests/test_views/test_order.py
+++ deltago/tests/test_views/test_order.py
@@ -34,7 +34,6 @@ class OrderViewTest(TestCase):
 
         self.order = Order.objects.get(pk=1)
 
-
         self.e_fee = {
             "subtotal": 8.36,
             "total": 13.36,
@@ -42,7 +41,7 @@ class OrderViewTest(TestCase):
             "exchange_rate": 5.0
         }
 
-        self.e_commodity_table_item = {
+        self.e_commodity_info_table_item = {
             "commodity": self.commodity,
             "price": 2.09,
             "quantity": 1,
@@ -50,58 +49,27 @@ class OrderViewTest(TestCase):
         }
 
 
-    def test_undelete_cartship(self):
-        cartshipes = [self.cartship1, self.cartship2]
-        for cartship in cartshipes:
-            order_service.undelete_cartship(cartship)
-            self.assertFalse(cartship.is_deleted)
-
-    def test_choose_cartship(self):
-        order_service.choose_cartship(self.cartship1)
-        self.assertTrue(self.cartship1.is_chosen)
-
-    def test_get_chosen_cartshipes(self):
-
+    def test_update_cartshipes(self):
         data = [
-            (self.checkboxes, False, self.cart, [self.cartship1]),
-            (self.checkboxes, True, self.cart, [self.cartship1, self.cartship2]),
+            ([1], False),
+            ([1], True),
         ]
-
-        for checkboxes, checkbox_all, cart, e_result in data:
-            chosens = order_service.get_chosen_cartshipes(checkboxes, checkbox_all, cart)
-            self.assertEqual(list(chosens), e_result)
-
-    def test_get_unchosen_cartshipes(self):
-        chosens = [self.cartship1]
-        unchosen = order_service.get_unchosen_cartshipes(self.all_cartshipes, chosens)
-        self.assertEqual(unchosen, [self.cartship2])
-
-    def test_update_quantities(self):
-        quantities = self.quantities
-        order_service.update_quantities(self.all_cartshipes, quantities)
-        for index, cartship in enumerate(self.all_cartshipes):
-            quantity = quantities[index]
-            self.assertEqual(cartship.quantity, quantity)
+        for checkboxes, checkbox_all in data:
+            updated_cartshipes = order_service.update_cartshipes(
+                self.cart, checkboxes, self.quantities, checkbox_all)
+            for index, updated in enumerate(updated_cartshipes):
+                self.assertEqual(updated.quantity, self.quantities[index])
+                if (updated.pk in checkboxes) or checkbox_all:
+                    self.assertEqual(updated.is_deleted, False)
+                    self.assertEqual(updated.is_chosen, True)
 
     def test_archive_cart(self):
         order_service.archive_cart(self.cart)
         self.assertTrue(self.cart.is_archived)
 
-    def test_new_cart_with_unchosens(self):
-        unchosens = [self.cartship2]
-        order_service.new_cart_with_unchosens(self.admin, unchosens)
-
-    def test_get_price(self):
-        price = order_service.get_price(self.commodity)
-        self.assertEqual(price, self.price)
-
-    def test_get_commodity_total(self):
-        commodity_total = order_service.get_commodity_total(self.cartship1)
-        self.assertEqual(commodity_total, self.price)
-
-    def test_get_subtotal(self):
-        subtotal = order_service.get_subtotal(self.all_cartshipes)
-        self.assertEqual(subtotal, 209*3)
+    def test_generate_order_serial_code(self):
+        serial_code = order_service.generate_order_serial_code(self.order)
+        self.assertEqual(len(serial_code), 20)
 
     def test_init_order(self):
         subtotal = 209*3
@@ -111,15 +79,54 @@ class OrderViewTest(TestCase):
         self.assertEqual(new_order.subtotal, subtotal)
         self.assertEqual(new_order.total, total)
 
-    def test_create_order_by_chosen(self):
+    def test_new_order_with_chosen(self):
         chosens = [self.cartship1]
         subtotal = 209
         total = 209 + 500
-        new_order = order_service.create_order_by_chosen(self.cart, chosens)
+        new_order = order_service.new_order_with_chosen(self.cart, chosens)
         self.assertEqual(new_order.user, self.cart.user)
         self.assertEqual(new_order.cart, self.cart)
         self.assertEqual(new_order.subtotal, subtotal)
         self.assertEqual(new_order.total, total)
+
+    def test_new_cart_with_unchosens(self):
+        unchosens = [self.cartship2]
+        new_cart = order_service.new_cart_with_unchosens(self.admin, unchosens)
+        self.assertEqual(self.cartship2.cart, new_cart)
+
+    def test_convert_fee(self):
+        amount = order_service.convert_fee(209)
+        self.assertEqual(amount, 2.09)
+
+    def test_get_order_show_fee(self):
+        fee = order_service.get_order_show_fee(self.order)
+        self.assertEqual(fee, self.e_fee)
+
+    def test_get_order_state_text(self):
+        text = order_service.get_order_state_text(self.order.state)
+        self.assertEqual(text, '未支付')
+
+    def test_get_order_basic_info(self):
+        order = order_service.get_order_basic_info(self.order)
+        self.assertEqual(order.fee, self.e_fee)
+        self.assertEqual(order.state_text, '未支付')
+
+    def test_get_commodity_info_table_item(self):
+        item = order_service.get_commodity_info_table_item(self.cartship1)
+        self.assertEqual(item, self.e_commodity_info_table_item)
+
+    def test_get_commodity_info_table(self):
+        data = [
+            (1, 2.09, 1, 2.09),
+            (2, 2.09, 2, 4.18)
+        ]
+        table = order_service.get_commodity_info_table(self.order)
+        for index, item in enumerate(table):
+            data_item = data[index]
+            self.assertEqual(item["commodity"].pk, data_item[0])
+            self.assertEqual(item["price"], data_item[1])
+            self.assertEqual(item["quantity"], data_item[2])
+            self.assertEqual(item["commodity_total"], data_item[3])
 
     def test_generate_order(self):
         user = self.admin
@@ -138,33 +145,3 @@ class OrderViewTest(TestCase):
                 self.assertEqual(new_order.user, user)
             except errors.EmptyCartError as e:
                 self.assertTrue(isinstance(e, errors.EmptyCartError))
-
-    def test_get_order_fee(self):
-        fee = order_service.get_order_fee(self.order)
-        self.assertEqual(fee, self.e_fee)
-
-    def test_get_commodity_table_item(self):
-        item = order_service.get_commodity_table_item(self.cartship1)
-        self.assertEqual(item, self.e_commodity_table_item)
-
-    def test_get_commodity_info_table(self):
-        data = [
-            (1, 2.09, 1, 2.09),
-            (2, 2.09, 2, 4.18)
-        ]
-        table = order_service.get_commodity_info_table(self.order)
-        for index, item in enumerate(table):
-            data_item = data[index]
-            self.assertEqual(item["commodity"].pk, data_item[0])
-            self.assertEqual(item["price"], data_item[1])
-            self.assertEqual(item["quantity"], data_item[2])
-            self.assertEqual(item["commodity_total"], data_item[3])
-
-    def test_get_order_state_text(self):
-        text = order_service.get_order_state_text(self.order.state)
-        self.assertEqual(text, '未支付')
-
-    def test_generate_order_serial_code(self):
-        serial_code = order_service.generate_order_serial_code(self.order)
-        self.assertEqual(len(serial_code), 20)
-
--- deltago/views/services/order_service.py
+++ deltago/views/services/order_service.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 from django.core.exceptions import ObjectDoesNotExist
+from django.http import Http404
 from django.utils import timezone
 import random
-
 
 from django.contrib.auth.models import User
 
@@ -10,14 +10,33 @@ from deltago.exceptions import errors
 
 from deltago.models import Order, Cart, Cartship
 
-from deltago.views.services import share_service
-from deltago.views.services import cart_service
+from deltago.views.services import cart_service, share_service
 
 from deltago.templatetags import date
 
 
-
 SHIP_FEE = 500
+
+# A. help functions
+
+# A1 generate order help functions
+
+# 1. update_quantities_checked_deleted_states_based_on_current_cart
+def update_cartshipes(current_cart, checkboxes, quantities, checkbox_all):
+    cartshipes = Cartship.objects.filter(cart=current_cart)
+    for index, cartship in enumerate(cartshipes):
+        is_chosen = checkbox_all or (cartship.pk in checkboxes)
+        cartship.is_chosen = is_chosen
+        if is_chosen and cartship.is_deleted:
+            cartship.is_deleted = False
+        cartship.quantity = quantities[index]
+        cartship.updated_date = timezone.now()
+        cartship.save()
+    return cartshipes
+
+def archive_cart(cart):
+    cart.is_archived = True
+    cart.save()
 
 def generate_order_serial_code(order):
     serial_code = ''
@@ -32,66 +51,7 @@ def generate_order_serial_code(order):
     serial_code += random_str + '0' * length
     return serial_code
 
-def undelete_cartship(cartship):
-    if cartship.is_deleted:
-        cartship.is_deleted = False
-        cartship.save()
-
-def choose_cartship(cartship):
-    cartship.is_chosen = True
-    cartship.save()
-
-def get_chosen_cartshipes(checkboxes, checkbox_all, current_cart):
-    if checkbox_all:
-        return Cartship.objects.filter(cart=current_cart)
-
-    chosens = []
-    for checkbox in checkboxes:
-        cartship = Cartship.objects.get(pk=checkbox)
-        undelete_cartship(cartship)
-        choose_cartship(cartship)
-        chosens.append(cartship)
-    return chosens
-
-def get_unchosen_cartshipes(all_cartshipes, chosens):
-    return list(set(all_cartshipes) - set(chosens))
-
-def update_quantities(all_cartshipes, quantities):
-    for index, cartship in enumerate(all_cartshipes):
-        quantity = quantities[index]
-        cartship.quantity = quantity
-        cartship.updated_date = timezone.now()
-        cartship.save()
-
-def archive_cart(cart):
-    cart.is_archived = True
-    cart.save()
-
-def new_cart_with_unchosens(user, unchosens):
-    new_cart = Cart(user=user)
-    new_cart.save()
-    for cartship in unchosens:
-        cartship.cart = new_cart
-        cartship.created_date = timezone.now()
-        cartship.updated_date = timezone.now()
-        cartship.save()
-
-def get_price(commodity):
-    price = commodity.price or commodity.was_price
-    return float(price) * 100
-
-def get_commodity_total(cartship):
-    commodity = cartship.commodity
-    price = get_price(commodity)
-    quantity = cartship.quantity
-    return price * quantity
-
-def get_subtotal(cartshipes):
-    subtotal = 0
-    for cartship in cartshipes:
-        subtotal += get_commodity_total(cartship)
-    return subtotal
-
+# create new order with chosen cartshipes
 def init_order(cart, subtotal, total):
     user = cart.user
     new_order = Order(
@@ -103,44 +63,28 @@ def init_order(cart, subtotal, total):
     new_order.save()
     return new_order
 
-def create_order_by_chosen(current_cart, chosens):
-    subtotal = get_subtotal(chosens)
+def new_order_with_chosen(current_cart, chosens):
+    subtotal = share_service.get_cartshipes_subtotal(chosens)
     total = subtotal + SHIP_FEE
     new_order = init_order(current_cart, subtotal, total)
     return new_order
-    
-def generate_order(user, checkboxes, quantities, checkbox_all):
-    current_cart = cart_service.user_current_cart(user)
-    # 1. update_all_cartship_quantity
-    all_cartshipes = Cartship.objects.filter(cart=current_cart)
-    update_quantities(all_cartshipes, quantities)
-    # 2. update_cartship_is_chosen 3. undeleted
-    chosens = get_chosen_cartshipes(checkboxes, checkbox_all, current_cart)
-    if not chosens:
-        raise errors.EmptyCartError("Choose at least one item.")
-    else:
-        # 4. mark cart archived
-        archive_cart(current_cart)
-        # 5. create new cart with unchosen cartshipes
-        unchosens = get_unchosen_cartshipes(all_cartshipes, chosens)
-        new_cart_with_unchosens(user, unchosens)
-        # 6. create order
-        new_order = create_order_by_chosen(current_cart, chosens)
-        return new_order
-        
 
+# create new cart with unchosen cartshipes
+def new_cart_with_unchosens(user, unchosens):
+    new_cart = Cart(user=user)
+    new_cart.save()
+    for cartship in unchosens:
+        cartship.cart = new_cart
+        cartship.created_date = timezone.now()
+        cartship.updated_date = timezone.now()
+        cartship.save()
+    return new_cart
 
-
-
-
-# Order display
-
-
-
+# A2 display order list help functions
 def convert_fee(amount):
     return amount / float(100)
 
-def get_order_fee(order):
+def get_order_show_fee(order):
     subtotal = convert_fee(order.subtotal)
     total = convert_fee(order.total)
     ship_fee = total - subtotal
@@ -152,26 +96,6 @@ def get_order_fee(order):
         "exchange_rate": exchange_rate
     }
 
-def get_commodity_table_item(cartship):
-    price = get_price(cartship.commodity)
-    show_price = convert_fee(price)
-    commodity_total = convert_fee(price * cartship.quantity)
-    return {
-        "commodity": cartship.commodity,
-        "price": show_price,
-        "quantity": cartship.quantity,
-        "commodity_total": commodity_total
-    }
-
-def get_commodity_info_table(order):
-    table = []
-    cart = order.cart
-    cartshipes = Cartship.objects.filter(cart=cart)
-    for cartship in cartshipes:
-        item = get_commodity_table_item(cartship)
-        table.append(item)
-    return table
-
 def get_order_state_text(state):
     state_text = {
         'U': '未支付',
@@ -182,28 +106,58 @@ def get_order_state_text(state):
     }
     return state_text[state]
 
-
-def get_order_details(order_id):
-    order = Order.objects.get(pk=order_id)
-    order = get_order_basic_info(order)
-    order.commodity_info_table = get_commodity_info_table(order)
-    return order
-
-# order list page
-
 def get_order_basic_info(order):
-    order.fee = get_order_fee(order)
+    order.fee = get_order_show_fee(order)
     order.state_text = get_order_state_text(order.state)
     return order
 
-def get_orders_basic_info(orders):
+def get_order_list_basic_info(orders):
     for order in orders:
         get_order_basic_info(order)
     return orders
 
-def order_list(user, page, per_page):
+
+# A3 display order details help functions
+def get_commodity_info_table_item(cartship):
+    price = convert_fee(share_service.get_commodity_price(cartship.commodity))
+    commodity_total = convert_fee(share_service.get_cartship_subtotal(cartship))
+    return {
+        "commodity": cartship.commodity,
+        "price": price,
+        "quantity": cartship.quantity,
+        "commodity_total": commodity_total
+    }
+
+def get_commodity_info_table(order):
+    table = []
+    cart = order.cart
+    cartshipes = Cartship.objects.filter(cart=cart)
+    for cartship in cartshipes:
+        item = get_commodity_info_table_item(cartship)
+        table.append(item)
+    return table
+
+# B exported view functions
+
+# B1 checkout view
+def generate_order(user, checkboxes, quantities, checkbox_all):
+    current_cart = cart_service.user_current_cart(user)
+    update_cartshipes(current_cart, checkboxes, quantities, checkbox_all)
+    chosens = Cartship.objects.filter(cart=current_cart, is_chosen=True)
+    if not chosens:
+        raise errors.EmptyCartError("Choose at least one item.")
+    else:
+        archive_cart(current_cart)
+        new_order = new_order_with_chosen(current_cart, chosens)
+        unchosens = Cartship.objects.filter(cart=current_cart, is_chosen=False)
+        if unchosens:
+            new_cart_with_unchosens(user, unchosens)
+        return new_order
+
+# B2 my_orders view
+def get_order_list(user, page, per_page):
     order_filter = Order.objects.filter(user=user)
-    data = get_orders_basic_info(order_filter)
+    data = get_order_list_basic_info(order_filter)
     orders = share_service.pagination(data, page, per_page)
     empty_tips = '还没有订单哦~'
     return {
@@ -211,3 +165,14 @@ def order_list(user, page, per_page):
         "paginations": orders,
         "empty_tips": empty_tips
     }
+
+# B3 order_details view
+def get_order_details(order_id):
+    try:
+        order = Order.objects.get(pk=order_id)
+    except ObjectDoesNotExist:
+        raise Http404('该订单不存在')
+
+    order = get_order_basic_info(order)
+    order.commodity_info_table = get_commodity_info_table(order)
+    return order
