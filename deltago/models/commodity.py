ecb87159d288b6c15d5c5c92060d05b96fcbc635
ainesmile <ainesmile@gmail.com>
2017-08-22 12:27:26 +1200

    modify commodity and details models

--- deltago/models/commodity.py
+++ deltago/models/commodity.py
@@ -5,13 +5,10 @@ from django.utils import timezone
 
 class Commodity(models.Model):
     name = models.CharField(max_length = 128)
+    price = models.IntegerField(default = 0, null = True, blank = True)
+    special_price = models.IntegerField(default = 0, null = True, blank = True)
     volume_size = models.CharField(max_length = 128, null = True, blank = True)
-    price = models.CharField(max_length = 128, null = True, blank = True)
-    was_price = models.CharField(max_length = 128, null = True, blank = True)
-    special_price = models.CharField(max_length = 128, null = True, blank = True)
     category = models.CharField(max_length = 20)
-    sub_category = models.CharField(max_length = 20)
-    stockcode = models.CharField(max_length = 20, unique=True)
     online_date = models.DateTimeField(default = timezone.now)
 
     def __str__(self):
@@ -19,9 +16,16 @@ class Commodity(models.Model):
 
 class Details(models.Model):
     commodity = models.ForeignKey('Commodity', on_delete=models.CASCADE)
-    pic_url = models.URLField(blank = True, null = True)
-    description = models.TextField(null = True, blank = True)
-    nutrition = models.TextField(max_length = 128, null = True, blank = True)
     ingredient = models.CharField(max_length = 128, null = True, blank = True)
-    claim = models.CharField(max_length = 128, null = True, blank = True)
-    endorsement = models.CharField(max_length = 128, null = True, blank = True)
+    serving_size = models.CharField(max_length = 16, null = True, blank = True)
+    servings = models.IntegerField(null = True, blank = True)
+    nutritions = models.TextField(max_length = 512, null = True, blank = True)
+    claims = models.CharField(max_length = 128, null = True, blank = True, default=None)
+    health_star_rating = models.IntegerField(null = True, blank = True, default=None)
+
+    weight = models.CharField(max_length = 16, null = True, blank = True, default=None)
+    made_in = models.CharField(max_length = 128, null = True, blank = True, default=None)
+    pic_url = models.URLField(blank = True, null = True, default=None)
+    description = models.TextField(null = True, blank = True, default=None)
+    endorsement = models.CharField(max_length = 128, null = True, blank = True, default=None)
+
