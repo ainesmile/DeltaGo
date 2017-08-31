fd45a4f9aedf51b4f7f8728a339ffac74dd911d5
ainesmile <ainesmile@gmail.com>
2017-08-31 16:08:31 +1200

    rename file comment and register all models

--- deltago/admin.py
+++ deltago/admin.py
@@ -3,11 +3,16 @@ from deltago.models import commodity
 from deltago.models import search
 from deltago.models import cart
 from deltago.models import order
+from deltago.models import comment
 
 admin.site.register(commodity.Commodity)
 admin.site.register(commodity.Details)
 admin.site.register(search.Search)
 admin.site.register(cart.Cart)
+admin.site.register(cart.Cartship)
 admin.site.register(order.Order)
 admin.site.register(order.Ship)
-admin.site.register(order.Payment)
+admin.site.register(comment.Comment)
+admin.site.register(comment.Reply)
+admin.site.register(comment.Reviewship)
+
