c2151832a227c4db051af5e2355e716ef19a10d6
ainesmile <ainesmile@gmail.com>
2017-09-03 22:10:17 +1200

    modify order pay url

--- deltago/urls/order.py
+++ deltago/urls/order.py
@@ -5,5 +5,5 @@ urlpatterns = [
     url(r'^checkout$', order.checkout, name='checkout'),
     url(r'^orders/mine$', order.my_orders, name='my_orders'),
     url(r'^orders/details/(?P<order_id>\w+)$', order.order_details, name='order_details'),
-    url(r'^order/pay/(?P<order_id>\w+)$', order.order_pay, name='order_pay'),
+    url(r'^order/(?P<order_id>\w+)/pay$', order.order_pay, name='order_pay'),
 ]
